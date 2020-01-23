f47d017f208c1f20653da78625ada12895dbee0b
Rebecca Burton <Rebecca.Burton@ghyston.com>
2020-01-23 19:02:01 +0000

    Fixed styling of tiles

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PlayerPiece } from 'GamePiece';
-import './square.css';
+import './tile.css';
 
 interface ITileProps {
     piece: PlayerPiece;
@@ -12,7 +12,9 @@ export const Tile = (props: ITileProps) => {
         ? `button ${props.piece.player.colour} tile-selected`
         : `button ${props.piece.player.colour} tile-normal`;
 
+    const orientation = props.piece.piece.isFlipped ? "R" : "I";
+
     return(
-        <button className={tileClassName}>{props.piece.piece.name}</button>
+        <button className={tileClassName}>{`${props.piece.piece.name} ${orientation}`}</button>
     )
 }
